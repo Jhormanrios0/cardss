53f3f9cd19754490dd997f1dfd18a055cad34498
jhorman <78864961+Jhormanrios0@users.noreply.github.com>
2024-08-14 17:00:13 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,17 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
   const searchInput = document.getElementById("searchInput");
   const cards = document.querySelectorAll(".card");
+  const noResultsMessage = document.createElement("p");
+
+  noResultsMessage.textContent = "No se encontraron resultados";
+  noResultsMessage.classList.add("no-results");
+  noResultsMessage.style.display = "none";
+  searchInput.parentNode.appendChild(noResultsMessage);
 
   searchInput.addEventListener("input", function () {
-    const searchQuery = this.value.toLowerCase();
+    const searchQuery = removeDiacritics(this.value.toLowerCase());
+    let found = false;
 
     cards.forEach(function (card) {
-      const title = card.querySelector(".card-title").textContent.toLowerCase();
+      const title = removeDiacritics(
+        card.querySelector(".card-title").textContent.toLowerCase()
+      );
       if (title.includes(searchQuery)) {
         card.style.display = "block";
+        found = true;
       } else {
         card.style.display = "none";
       }
     });
+
+    if (!found) {
+      noResultsMessage.style.display = "block";
+    } else {
+      noResultsMessage.style.display = "none";
+    }
   });
+
+  function removeDiacritics(text) {
+    return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  }
 });
